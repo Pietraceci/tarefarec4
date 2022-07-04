a16545a1114ccae1b0651e9ef38fd8f581ab3f47
Pietraceci <pietracecily@gmail.com>
2022-07-04 01:03:14 -0400

    Testando dados

--- src/Sites.jsx
+++ src/Sites.jsx
@@ -1,16 +1,26 @@
 import './Sites.css'
 function Sites(){
     return(
-      <div className='container0'>
-        <div className='container1'>
+      <div className='container'>
+
+        <div className='container0'>
           <div className='img'>
             <img src='https://i2.wp.com/gi-builds.sfo3.digitaloceanspaces.com/characters/kaeya/image.png?strip=all&quality=100&w=256'/>
           </div>
           <div className='text'>
             Teste Kaeya Alberich
           </div>
-          
         </div>
+
+        <div className='container0'>
+          <div className='img'>
+            <img src='https://i2.wp.com/gi-builds.sfo3.digitaloceanspaces.com/characters/albedo/image.png?strip=all&quality=100&w=256'/>
+          </div>
+          <div className='text'>
+            Teste Albedo
+          </div>
+        </div>
+
       </div>
     );
 
