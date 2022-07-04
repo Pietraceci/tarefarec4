658ce3d87a1bb108eaabc7557f9e4f36a58f9cff
Pietraceci <pietracecily@gmail.com>
2022-07-04 00:58:21 -0400

    Criando primeiras linhas de código, códigos iniciais

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import Sites from './Sites.jsx'
 
 function App(){
     return(
-        
+        <div>
+            <Sites/>
+        </div>
     )
 }
 export default App;
--- src/Sites.jsx
+++ src/Sites.jsx
@@ -1,8 +1,18 @@
 import './Sites.css'
 function Sites(){
     return(
-
-    )
+      <div className='container0'>
+        <div className='container1'>
+          <div className='img'>
+            <img src='https://i2.wp.com/gi-builds.sfo3.digitaloceanspaces.com/characters/kaeya/image.png?strip=all&quality=100&w=256'/>
+          </div>
+          <div className='text'>
+            Teste Kaeya Alberich
+          </div>
+          
+        </div>
+      </div>
+    );
 
   }
-  export default News;
+  export default Sites;
