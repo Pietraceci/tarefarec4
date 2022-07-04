dcce0f6a068edc67fd3f19d20c884e8e7865cefa
Pietraceci <pietracecily@gmail.com>
2022-07-04 01:56:33 -0400

    Customizando e adicionando valores 2

--- src/Sites.jsx
+++ src/Sites.jsx
@@ -5,7 +5,7 @@ function Sites(){
 
         <div className='container0'>
           <div className='img'>
-            <img src='https://i2.wp.com/gi-builds.sfo3.digitaloceanspaces.com/characters/kaeya/image.png?strip=all&quality=100&w=256'/>
+            <img src='https://suap.ifsp.edu.br/static/comum/img/favicon.png'/>
           </div>
           <div className='text'>
             Início
@@ -14,7 +14,7 @@ function Sites(){
 
         <div className='container0'>
           <div className='img'>
-            <img src='https://i2.wp.com/gi-builds.sfo3.digitaloceanspaces.com/characters/albedo/image.png?strip=all&quality=100&w=256'/>
+            <img src='https://www.freelogovectors.net/wp-content/uploads/2022/01/globo-com-logo-freelogovectors.net_.png'/>
           </div>
           <div className='text'>
             globo.com
@@ -23,7 +23,7 @@ function Sites(){
 
         <div className='container0'>
           <div className='img'>
-            <img src='https://i2.wp.com/gi-builds.sfo3.digitaloceanspaces.com/characters/kaeya/image.png?strip=all&quality=100&w=256'/>
+            <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/2048px-Facebook_f_logo_%282019%29.svg.png'/>
           </div>
           <div className='text'>
             Facebook
@@ -32,7 +32,7 @@ function Sites(){
 
         <div className='container0'>
           <div className='img'>
-            <img src='https://i2.wp.com/gi-builds.sfo3.digitaloceanspaces.com/characters/albedo/image.png?strip=all&quality=100&w=256'/>
+            <img src='https://opensource.apple.com/source/cups/cups-136/cups/doc/images/cups-large.gif'/>
           </div>
           <div className='text'>
             Home
@@ -41,7 +41,7 @@ function Sites(){
 
         <div className='container0'>
           <div className='img'>
-            <img src='https://i2.wp.com/gi-builds.sfo3.digitaloceanspaces.com/characters/kaeya/image.png?strip=all&quality=100&w=256'/>
+            <img src='https://yt3.ggpht.com/ytc/AKedOLQ9HrY8e1gukgsM6EfjsEFw6okVXu9omad8Cz6LXg=s900-c-k-c0x00ffffff-no-rj'/>
           </div>
           <div className='text'>
             Mirim
@@ -50,7 +50,7 @@ function Sites(){
 
         <div className='container0'>
           <div className='img'>
-            <img src='https://i2.wp.com/gi-builds.sfo3.digitaloceanspaces.com/characters/albedo/image.png?strip=all&quality=100&w=256'/>
+            <img src='https://yt3.ggpht.com/ytc/AKedOLQ9HrY8e1gukgsM6EfjsEFw6okVXu9omad8Cz6LXg=s900-c-k-c0x00ffffff-no-rj'/>
           </div>
           <div className='text'>
             AVA
@@ -59,7 +59,7 @@ function Sites(){
 
         <div className='container0'>
           <div className='img'>
-            <img src='https://i2.wp.com/gi-builds.sfo3.digitaloceanspaces.com/characters/kaeya/image.png?strip=all&quality=100&w=256'/>
+            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSycGDFabnh4YUS4mrwSabSqtsLWB4eeIOKb5cWJY3x2lzHGKU4242qQUIeRLNIK7dfAVc&usqp=CAU'/>
           </div>
           <div className='text'>
             SEI/ IFRO
@@ -68,7 +68,7 @@ function Sites(){
 
         <div className='container0'>
           <div className='img'>
-            <img src='https://i2.wp.com/gi-builds.sfo3.digitaloceanspaces.com/characters/albedo/image.png?strip=all&quality=100&w=256'/>
+            <img src='https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcS9rLPmmwC-2jD18U7uhpXqaCmuYqhDje5mh7jgOuUYY5jQbd2M'/>
           </div>
           <div className='text'>
             E
@@ -77,7 +77,7 @@ function Sites(){
 
         <div className='container0'>
           <div className='img'>
-            <img src='https://i2.wp.com/gi-builds.sfo3.digitaloceanspaces.com/characters/kaeya/image.png?strip=all&quality=100&w=256'/>
+            <img src='https://yt3.ggpht.com/ytc/AKedOLQ9HrY8e1gukgsM6EfjsEFw6okVXu9omad8Cz6LXg=s900-c-k-c0x00ffffff-no-rj'/>
           </div>
           <div className='text'>
             Mirim
@@ -86,7 +86,7 @@ function Sites(){
 
         <div className='container0'>
           <div className='img'>
-            <img src='https://i2.wp.com/gi-builds.sfo3.digitaloceanspaces.com/characters/albedo/image.png?strip=all&quality=100&w=256'/>
+            <img src='https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSeqR_yYu6QFz2_Gb11ekIcE9U9r7NKvcE_C4d-xTVhpDrlb_XA'/>
           </div>
           <div className='text'>
             Add short...
